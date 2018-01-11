omni-compiler/omni-compiler

commit 31b471844670316df2a15c06f3d76616c9b95609

fix #359

F-FrontEnd/src/F-compile-decl.c
@@ -2256,6 +2256,7 @@ declare_id_type(ID id, TYPE_DESC tp)
         /* override implicit declared type */
         TYPE_ATTR_FLAGS(tp) |= TYPE_ATTR_FLAGS(tq);
         replace_or_assign_type(id_type, tp);
+        TYPE_ATTR_FLAGS(ID_TYPE(id)) |= TYPE_ATTR_FLAGS(tp);
         return;
     }
 

F-FrontEnd/src/F-compile.c
@@ -6847,7 +6847,7 @@ compile_ALLOCATE_DEALLOCATE_statement(expr x)
             case F95_MEMBER_REF:
             case F_VAR:
             case ARRAY_REF:
-                case XMP_COARRAY_REF:
+	    case XMP_COARRAY_REF:
                 if(isVarSetTypeAttr(ev,
                     TYPE_ATTR_POINTER | TYPE_ATTR_ALLOCATABLE) == FALSE) {
                     error("argument is not a pointer nor allocatable type");

omni-compiler/xcodeml-tools

commit 04a2b4069dc2cec6e14d9651f4c97d06f4ddd77a

Fix a regression in 31b471844670

F-FrontEnd/src/F-compile-decl.c
@@ -2234,7 +2234,7 @@ declare_id_type(ID id, TYPE_DESC tp)
         if (ID_TYPE(id) == NULL) {
             if (!IS_PROCEDURE_TYPE(tp) || IS_PROCEDURE_POINTER(tp)) {
                 /* change the type into function type */
-                ID_TYPE(id) = function_type(tp);
+                *id_type = function_type(tp);
             } else {
                 ID_TYPE(id) = tp;
                 return;
@@ -2263,7 +2263,6 @@ declare_id_type(ID id, TYPE_DESC tp)
         /* override implicit declared type */
         TYPE_ATTR_FLAGS(tp) |= TYPE_ATTR_FLAGS(tq);
         replace_or_assign_type(id_type, tp);
-        TYPE_ATTR_FLAGS(ID_TYPE(id)) |= TYPE_ATTR_FLAGS(tp);
         return;
     }
 

F-FrontEnd/src/F-compile-expr.c
@@ -1900,6 +1900,7 @@ compile_array_ref(ID id, expv vary, expr args, int isLeft) {
 
     if (id != NULL) {
         vary = expv_sym_term(F_VAR, ID_TYPE(id), ID_SYM(id));
+
         ID_ADDR(id) = vary;
 
         tp = ID_TYPE(id);
@@ -1910,6 +1911,8 @@ compile_array_ref(ID id, expv vary, expr args, int isLeft) {
         if (id != NULL && ID_CLASS(id) == CL_PROC && PROC_CLASS(id) == P_THISPROC) {
             tp = FUNCTION_TYPE_RETURN_TYPE(tp);
         }
+
+        EXPV_TYPE(vary) = tp;
 
         if (TYPE_N_DIM(tp) < n) {
             error_at_node(args, "too large dimension, %d.", n);

F-FrontEnd/test/testdata/allocate_to_function_result.f90
@@ -1,0 +1,4 @@
+       FUNCTION f()
+         REAL, ALLOCATABLE :: f(:)
+         ALLOCATE(f(1:3))
+       END