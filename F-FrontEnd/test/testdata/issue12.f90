ea1a229dff59f13b01412820204233cb5d829289
Valentin Clement <clementval@gmail.com>
2018-05-22 10:04:55 +0200

    Enhance test data

--- F-FrontEnd/test/testdata/issue12.f90
+++ F-FrontEnd/test/testdata/issue12.f90
@@ -1,6 +1,10 @@
 module mod1
-  integer, parameter :: sp = selected_real_kind(6,37)
+  integer, parameter :: r1 = selected_real_kind()
+  integer, parameter :: sp = selected_real_kind(6, 37)
   integer, parameter :: dp = selected_real_kind(12,307)
+
+  integer, parameter :: i1 = selected_int_kind(4);
+  integer, parameter :: i2 = selected_int_kind(8);
 
 contains
 
@@ -12,8 +16,17 @@ contains
         print*,'arg is sp'
       type is(real(dp))
         print*,'arg is dp'
+      type is(integer(i1))
+        print*,'arg is i1'
+      type is(integer(i2))
+        print*,'arg is i2'  
     end select
    
+   select type(arg1)
+      type is(real(r1)) 
+        print*,'arg is r1'
+      type is(real(dp))
+        print*,'arg is dp'
+    end select
   end subroutine sub1
-
 end module mod1
