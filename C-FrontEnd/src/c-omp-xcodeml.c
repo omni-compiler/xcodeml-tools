d48dc2d19a9b5c78f2a4c7a04f33257b37af5a86
Masahiro Nakao <masahiro.nakao@riken.jp>
2018-07-10 18:02:11 +0900

    Support OpenMP declare target directive

--- C-FrontEnd/src/c-omp-xcodeml.c
+++ C-FrontEnd/src/c-omp-xcodeml.c
@@ -9,6 +9,7 @@
 
 void outx_OMP_Clause(FILE *fp, int indent, CExprOfList* clause);
 void out_OMP_name_list(FILE *fp,int indent, CExprOfList *list);
+void out_ACC_subscript(FILE *fp,int indent, CExpr *subscript);
 
 void
 out_OMP_PRAGMA(FILE *fp, int indent, int pragma_code, CExpr* expr)
@@ -33,8 +34,8 @@ out_OMP_PRAGMA(FILE *fp, int indent, int pragma_code, CExpr* expr)
 
     outxPrint(fp,indent1,"<list>\n");
     EXPR_FOREACH(ite, clauseList){
-	CExpr *node = EXPR_L_DATA(ite);
-	outx_OMP_Clause(fp,indent1+1,(CExprOfList *)node);
+      CExpr *node = EXPR_L_DATA(ite);
+      outx_OMP_Clause(fp,indent1+1,(CExprOfList *)node);
     }
     outxPrint(fp,indent1,"</list>\n");
 
@@ -62,9 +63,18 @@ outx_OMP_Clause(FILE *fp, int indent, CExprOfList* clause)
 
   case OMP_DIR_IF:
   case OMP_DIR_NUM_THREADS:
-  case OMP_COLLAPSE:
       outxContext(fp,indent1+1,arg);
       break;
+
+  case OMP_TARGET_DEVICE:
+    {
+      CCOL_DListNode *ite;
+      EXPR_FOREACH(ite, arg){
+	CExpr *node = EXPR_L_DATA(ite);
+	out_ACC_subscript(fp,indent1+1,node);
+      }
+      break;
+    }
 
   case OMP_DIR_SCHEDULE:
       outxPrint(fp,indent1,"<list>\n");
@@ -80,9 +90,9 @@ outx_OMP_Clause(FILE *fp, int indent, CExprOfList* clause)
       break;
       
   default:
-      namelist = (CExprOfList *)arg;
-      if(EXPR_L_SIZE(namelist) != 0)
-	  out_OMP_name_list(fp, indent, namelist);
+    namelist = (CExprOfList *)arg;
+    if(EXPR_L_SIZE(namelist) != 0)
+      out_OMP_name_list(fp, indent1, namelist);
   }
   outxPrint(fp,indent,"</list>\n");
 }
@@ -93,10 +103,9 @@ void out_OMP_name_list(FILE *fp,int indent, CExprOfList *list)
     CCOL_DListNode *ite;
     outxPrint(fp,indent,"<list>\n");
     EXPR_FOREACH(ite, list) {
-	CExpr *node = EXPR_L_DATA(ite);
-	// outx_IDENT(fp,indent1+1,(CExprOfSymbol *)node);
-	outxPrint(fp,indent1,"<Var>%s</Var>\n",
-		  ((CExprOfSymbol *)node)->e_symName);
+      CExpr *node = EXPR_L_DATA(ite);
+      outxPrint(fp,indent1,"<Var>%s</Var>\n",
+		((CExprOfSymbol *)node)->e_symName);
     }
     outxPrint(fp,indent,"</list>\n");
 }
@@ -104,52 +113,58 @@ void out_OMP_name_list(FILE *fp,int indent, CExprOfList *list)
 char *ompDirectiveName(int c)
 {
   switch(c){
-  case OMP_PARALLEL:  return "PARALLEL";
-  case OMP_FOR: return "FOR";
-  case OMP_SECTIONS: return "SECTIONS";
-  case OMP_SECTION: return "SECTION";
-  case OMP_SINGLE: return "SINGLE";
-  case OMP_MASTER: return "MASTER";
-  case OMP_CRITICAL: return "CRITICAL";
-  case OMP_BARRIER: return "BARRIER";
-  case OMP_ATOMIC: return "ATOMIC";
-  case OMP_FLUSH:return "FLUSH";
-  case OMP_ORDERED:return "ORDERED";
-  case OMP_THREADPRIVATE:return "THREADPRIVATE";
-  case OMP_PARALLEL_FOR:return "PARALLEL_FOR";
-  case OMP_PARALLEL_SECTIONS:return "PARALLEL_SECTIONS";
-  default: return "OMP???";
+  case OMP_PARALLEL:          return "PARALLEL";
+  case OMP_FOR:               return "FOR";
+  case OMP_SECTIONS:          return "SECTIONS";
+  case OMP_SECTION:           return "SECTION";
+  case OMP_SINGLE:            return "SINGLE";
+  case OMP_MASTER:            return "MASTER";
+  case OMP_CRITICAL:          return "CRITICAL";
+  case OMP_BARRIER:           return "BARRIER";
+  case OMP_ATOMIC:            return "ATOMIC";
+  case OMP_FLUSH:             return "FLUSH";
+  case OMP_ORDERED:           return "ORDERED";
+  case OMP_THREADPRIVATE:     return "THREADPRIVATE";
+  case OMP_PARALLEL_FOR:      return "PARALLEL_FOR";
+  case OMP_PARALLEL_SECTIONS: return "PARALLEL_SECTIONS";
+  case OMP_DECLARE_TARGET:    return "DECLARE_TARGET";
+  default:
+    return "OMP???";
   }
 }
 
 char *ompClauseName(int c)
 {
   switch(c){
-  case OMP_DATA_DEFAULT: return "DATA_DEFAULT";
-  case OMP_DATA_PRIVATE: return "DATA_PRIVATE";
-  case OMP_DATA_SHARED: return "DATA_SHARED";
-  case OMP_DATA_FIRSTPRIVATE: return "DATA_FIRSTPRIVATE";
-  case OMP_DATA_LASTPRIVATE: return "DATA_LASTPRIVATE";
-  case OMP_DATA_COPYIN: return "DATA_COPYIN";
+  case OMP_DATA_DEFAULT:          return "DATA_DEFAULT";
+  case OMP_DATA_PRIVATE:          return "DATA_PRIVATE";
+  case OMP_DATA_SHARED:           return "DATA_SHARED";
+  case OMP_DATA_FIRSTPRIVATE:     return "DATA_FIRSTPRIVATE";
+  case OMP_DATA_LASTPRIVATE:      return "DATA_LASTPRIVATE";
+  case OMP_DATA_COPYIN:           return "DATA_COPYIN";
 
-  case OMP_DATA_REDUCTION_PLUS: return "DATA_REDUCTION_PLUS";
-  case OMP_DATA_REDUCTION_MINUS: return "DATA_REDUCTION_MINUS";
-  case OMP_DATA_REDUCTION_MUL: return "DATA_REDUCTION_MUL";
+  case OMP_DATA_REDUCTION_PLUS:   return "DATA_REDUCTION_PLUS";
+  case OMP_DATA_REDUCTION_MINUS:  return "DATA_REDUCTION_MINUS";
+  case OMP_DATA_REDUCTION_MUL:    return "DATA_REDUCTION_MUL";
   case OMP_DATA_REDUCTION_BITAND: return "DATA_REDUCTION_BITAND";
-  case OMP_DATA_REDUCTION_BITOR: return "DATA_REDUCTION_BITOR";
+  case OMP_DATA_REDUCTION_BITOR:  return "DATA_REDUCTION_BITOR";
   case OMP_DATA_REDUCTION_BITXOR: return "DATA_REDUCTION_BITXOR";
   case OMP_DATA_REDUCTION_LOGAND: return "DATA_REDUCTION_LOGAND";
-  case OMP_DATA_REDUCTION_LOGOR: return "DATA_REDUCTION_LOGOR";
-  case OMP_DATA_REDUCTION_MIN: return "DATA_REDUCTION_MIN";
-  case OMP_DATA_REDUCTION_MAX: return "DATA_REDUCTION_MAX";
+  case OMP_DATA_REDUCTION_LOGOR:  return "DATA_REDUCTION_LOGOR";
+  case OMP_DATA_REDUCTION_MIN:    return "DATA_REDUCTION_MIN";
+  case OMP_DATA_REDUCTION_MAX:    return "DATA_REDUCTION_MAX";
 
-  case OMP_DIR_ORDERED: return "DIR_ORDERED";
-  case OMP_DIR_IF: return "DIR_IF";
-  case OMP_DIR_NOWAIT: return "DIR_NOWAIT";
-  case OMP_DIR_SCHEDULE: return "DIR_SCHEDULE";
-  case OMP_DIR_NUM_THREADS: return "DIR_NUM_THREADS";
-  case OMP_COLLAPSE: return "COLLAPSE";
-  default:  return "???OMP???";
+  case OMP_DIR_ORDERED:           return "DIR_ORDERED";
+  case OMP_DIR_IF:                return "DIR_IF";
+  case OMP_DIR_NOWAIT:            return "DIR_NOWAIT";
+  case OMP_DIR_SCHEDULE:          return "DIR_SCHEDULE";
+  case OMP_DIR_NUM_THREADS:       return "DIR_NUM_THREADS";
+  case OMP_COLLAPSE:              return "COLLAPSE";
+  case OMP_DECLARE_TARGET_TO:     return "DECLARE_TARGET_TO";
+  case OMP_TARGET_DEVICE:         return "TARGET_DEVICE";
+  case OMP_TARGET_LAYOUT:         return "TARGET_LAYOUT";
+    
+  default:                        return "???OMP???";
   }
 }
 
