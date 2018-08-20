43477922bc8262e9c15c74354583fdf93fc309eb
Hitoshi Murai <h-murai@riken.jp>
2018-08-20 16:49:45 +0900

    fix omni-compiler/omni-compiler#502

--- F-FrontEnd/src/F-compile-decl.c
+++ F-FrontEnd/src/F-compile-decl.c
@@ -1059,7 +1059,8 @@ declare_function(ID id)
                     }
                     PROC_CLASS(id) = P_UNDEFINEDPROC;
                 } else {
-                    if (!TYPE_IS_PROCEDURE(tp) || (TYPE_IS_PROCEDURE(tp) && TYPE_REF(tp) != NULL)) {
+		    if (!IS_SUBR(tp) &&
+			(!TYPE_IS_PROCEDURE(tp) || (TYPE_IS_PROCEDURE(tp) && TYPE_REF(tp) != NULL))) {
                         ID_TYPE(id) = function_type(tp);
                         TYPE_UNSET_SAVE(FUNCTION_TYPE_RETURN_TYPE(ID_TYPE(id)));
                         if(!TYPE_IS_PROCEDURE(tp)) { // undefined procedure
