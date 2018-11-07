0a767a686b603aef7c606b8970f0ea1e9a0974e0
Valentin Clement <clementval@gmail.com>
2018-11-07 11:38:25 +0100

    Add test data

---
+++ F-FrontEnd/test/testdata/issue83.f90
@@ -0,0 +1,12 @@
+MODULE mod1
+  USE issue83, ONLY: p_gatherv
+  IMPLICIT NONE
+  PRIVATE
+   INTERFACE p_gatherv
+    MODULE PROCEDURE p_gatherv_1d
+  END INTERFACE p_gatherv
+ CONTAINS
+  SUBROUTINE p_gatherv_1d()
+  END SUBROUTINE p_gatherv_1d
+END MODULE mod1
+
