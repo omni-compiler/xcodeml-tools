0c336bf76e7202e08707ec4f1af1f5f3fa1e7f08
Valentin Clement <clementval@gmail.com>
2018-12-06 09:46:43 +0000

    Add test data

---
+++ F-FrontEnd/test/testdata/_issue123.f90
@@ -0,0 +1,27 @@
+module fckit_shared_ptr_module
+implicit none
+private
+
+public fckit_shared_ptr
+public fckit_shared_ptr__final_auto
+
+type :: fckit_shared_ptr
+  logical, private :: return_value = .false.
+
+contains
+  procedure, public :: final => fckit_shared_ptr__final
+
+  final :: fckit_shared_ptr__final_auto
+end type
+contains
+
+subroutine fckit_shared_ptr__final_auto(this)
+  type(fckit_shared_ptr), intent(inout) :: this
+  call this%final()
+end subroutine
+
+subroutine fckit_shared_ptr__final(this)
+  class(fckit_shared_ptr), intent(inout) :: this
+end subroutine
+
+end module
