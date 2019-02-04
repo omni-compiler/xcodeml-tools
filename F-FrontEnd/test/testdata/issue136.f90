5b798c619ff4ce42a077aaa7b4148bd1603e02b5
Valentin Clement <clementval@gmail.com>
2019-02-04 11:35:57 +0100

    Update test case with more types

--- F-FrontEnd/test/testdata/issue136.f90
+++ F-FrontEnd/test/testdata/issue136.f90
@@ -1,10 +1,28 @@
 module mod1 
 contains
 
-double complex function fct0()
+complex function cfct()
 end function
 
-complex*16 function fct1()
+double complex function dcfct()
+end function
+
+real*8 function rfct8()
+end function
+
+real*16 function rfct16()
+end function
+
+integer*8 function ifct8()
+end function
+
+integer*16 function ifct16()
+end function
+
+complex*8 function cfct8()
+end function
+
+complex*16 function cfct16()
 end function
 
 end module mod1
