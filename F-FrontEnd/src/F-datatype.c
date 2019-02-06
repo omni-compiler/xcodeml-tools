5c0603e5a7dab0675e15a9b2e077bf1e095f80b2
Valentin Clement <clementval@gmail.com>
2019-02-06 13:12:17 +0100

    Add more constant to type_kind_is_constant

--- F-FrontEnd/src/F-datatype.c
+++ F-FrontEnd/src/F-datatype.c
@@ -1129,7 +1129,14 @@ int type_kind_is_constant(TYPE_DESC tp)
     if (type_has_kind(tp)) {
         expv kind = type_get_kind(tp);
         switch (EXPR_CODE(kind)) {
-            case INT_CONSTANT:
+            case STRING_CONSTANT:
+	        case INT_CONSTANT:
+            case FLOAT_CONSTANT:
+	        case COMPLEX_CONSTANT:
+	        case F_TRUE_CONSTANT:
+	        case F_FALSE_CONSTANT:
+	        case F_DOUBLE_CONSTANT:
+	        case F_QUAD_CONSTANT:
                 return TRUE;
             default:
                 return FALSE;
