288849c2fd4650457b829acb66f604cf8840486e
kosuga <kosuga@sra.co.jp>
2021-02-25 15:09:58 +0900

    'end declare target' does not support nested and local declarations.

--- C-FrontEnd/src/c-omp-pragma.c
+++ C-FrontEnd/src/c-omp-pragma.c
@@ -714,8 +714,13 @@ static int parse_OMP_end_pragma() {
     if (PG_IS_IDENT("target")) {
       /* declare target ... end declare target */
       pg_get_token();
-      pg_OMP_pragma = OMP_DECLARE_TARGET_END;
-      ret = PRAGMA_EXEC;
+      if (expr_num_nested_decl == 0ULL) {
+        pg_OMP_pragma = OMP_DECLARE_TARGET_END;
+        ret = PRAGMA_EXEC;
+      } else {
+        addError(NULL, "OMP: OpenMP declare directive: "
+                 "Nested and local declarations are not supported.");
+      }
     } else {
       addError(NULL, "OMP: OpenMP declare directive: "
                "unsupported : '%s'", pg_tok_buf);
